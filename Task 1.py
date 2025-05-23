3f003c6fb587874192efb2846e429420e5737686
satyam904 <sahusatyam908@gmail.com>
2025-05-23 21:27:56 +0530

    Task 1, 2 submission

---
+++ Task 1.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter a number : "))
+num2 = int(input("Enter a number : "))
+
+print(num1 + num2)
+
+num1 = int(input("Enter a number : "))
+num2 = int(input("Enter a number : "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(f'Addition : {add}')
+print(f'Subtraction : {sub}')
+print(f'Multiplication : {mul}')
+print(f'division : {div}')
