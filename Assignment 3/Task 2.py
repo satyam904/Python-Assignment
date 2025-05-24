9e6a0e15f708117d829e04949d9430f4a523828a
satyam904 <sahusatyam908@gmail.com>
2025-05-24 18:35:10 +0530

    Task 1, 2 submission

---
+++ Assignment 3/Task 2.py
@@ -0,0 +1,11 @@
+import math
+
+x = int(input("Enter a number : "))
+
+print("logarithm :", math.log(x))
+
+square_root = math.sqrt(x)
+print(f"Square root : {square_root}")
+
+sine_value = math.sin(x)
+print(f"sine : {sine_value}")
